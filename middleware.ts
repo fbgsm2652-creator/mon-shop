8fd1dd6668bde5c5da0550bf1ccc4ea180e1c05b
RENW-Admin <fbgsm2652@gmail.com>
2026-02-20 09:57:24 +0100

    fix: ignore system pages in middleware

--- middleware.ts
+++ middleware.ts
@@ -6,17 +6,20 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
+  // 1. On récupère l'objet auth
+  const authObj = await auth();
 
-  if (!userId && isProtectedRoute(req)) {
-    // Si l'utilisateur n'est pas connecté et essaie d'accéder à une route protégée
-    return (await auth()).redirectToSignIn();
+  // 2. Si c'est une route protégée ET que l'utilisateur n'est pas connecté
+  if (isProtectedRoute(req) && !authObj.userId) {
+    return authObj.redirectToSignIn();
   }
 });
 
 export const config = {
   matcher: [
+    // On ignore les fichiers internes de Next.js et les fichiers statiques
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // On force le middleware à s'exécuter sur les routes API
     '/(api|trpc)(.*)',
   ],
 };
